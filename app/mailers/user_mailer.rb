1b08dc37f8ac07a2488d09ac7d0987a3e5aba0f8
Mandeep05 <mandeep052@gmail.com>
2013-10-30 14:39:30 +0530

    mail fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ActionMailer::Base
   default from: "mandeep052@gmail.com"
   def test()
-  	mail to: "mandeep052@gmail.com", subject: "test", css: "email"
+  	mail to: "mandeep052@gmail.com", subject: "test", css: ["email","phone","desktop"]
   end
 end
